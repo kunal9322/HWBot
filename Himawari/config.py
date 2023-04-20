7a38e39c45c14796491183ec72685ff089db8b82
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-04-20 19:49:00 +0530

    Update config.py
    Signed-off-by: Kunal <113813635+kunal9322@users.noreply.github.com>

--- Himawari/config.py
+++ Himawari/config.py
@@ -52,34 +52,34 @@ class Config(object):
     #you can change these 
     INFOPIC=True #picture while doing /info
     STRICT_GBAN=True
-    API_ID=681 ##api id from my.telegram.org
+    API_ID=16743442 ##api id from my.telegram.org
     APP_ID=6281 #same as API_ID
-    API_HASH="45aabfacadsdsds" ##api hash from my.telegram.org
+    API_HASH="12bbd720f4097ba7713c5e40a11dfd2a" ##api hash from my.telegram.org
     APP_HASH="45aabfadsdsd" #same as API_HASH
     BL_CHATS=[1] #chats to be blacklisted
-    MONGO_DB_URL="mongodb+srv://mesdsdwhidsdsgodb.net/?retryWrites=true&w=majority" ##mongo database link (necessary)
+    MONGO_DB_URL="mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority" ##mongo database link (necessary)
     DB_URL2="mongodb+srv://meowhisswswuj7.mongodb.net/?retryWrites=true&w=majority" #mongo db (not necessary)
     DB_URL="postgresql://wdsdsw@ldsas/wasassddb" #postgres sql database link
     REDIS_URL="redis://dsds3@reddsdsuth-1-1.ec2.cloud.redislabs.com:154dsd/Arssddsdsb" #redis database url from redislabs.com
-    TOKEN="54dssdV8e59Odo" #bot token from @BotFather
-    DEV_USERS=[5437915218] #developers id
-    DRAGONS=[9656] #sudo users id
-    DEMONS=[1909] #support user ids
+    TOKEN="5481492613:AAGEoed5DnY2AtZ86nVLaev59Ty8cMa3fB4" #bot token from @BotFather
+    DEV_USERS=[5481492613] #developers id
+    DRAGONS=[778393824] #sudo users id
+    DEMONS=[11045939902] #support user ids
     TIGERS=[1] #commas for multiple ids
     WOLVES=[2112, 1212] #commas for multiple ids 
-    DONATION_LINK="https://www.paypal.me/PaulSonOfLars" #u can change with yours
-    EVENT_LOGS=-100159 #channel id for gban logs
-    JOIN_LOGGER=-1001523  #log channel/group id
-    OWNER_ID=5437915218 #owner id in integer
-    ERROR_LOGS=-1001 #support group id
-    BOT_ID = 2321 #id of bot in integer value
-    BOT_NAME="Himawari" #your bot name
+    DONATION_LINK="MH17_KUNAL.t.me" #u can change with yours
+    EVENT_LOGS=-1001935950378 #channel id for gban logs
+    JOIN_LOGGER=-1001935950378  #log channel/group id
+    OWNER_ID=6091170475 #owner id in integer
+    ERROR_LOGS=-1001453145062 #support group id
+    BOT_ID = 5481492613 #id of bot in integer value
+    BOT_NAME="ᴠᴇʀᴍᴇɪʟ" #your bot name
     ARQ_API_KEY="SLSFXSsdUXNSMH-ARQ" #ARQ api key from @ARQRobot
     ARQ_API_URL="arq.hamker.dev" #arq link
-    SUPPORT_CHAT="Ignidsn" #support group username without @
-    OWNER_USERNAME="arsshh" #owner username without @
-    UPDATES_CHANNEL="Igsd" #Updates/News Channel username without @
-    BOT_USERNAME="Himawari_Robot" #bot username without @
+    SUPPORT_CHAT="Anime_Krew" #support group username without @
+    OWNER_USERNAME="The_Nanamii" #owner username without @
+    UPDATES_CHANNEL="Campus_Bot_Updates" #Updates/News Channel username without @
+    BOT_USERNAME="TheVermeil_Bot" #bot username without @
     REM_BG_API_KEY="K2dsdsYma6cZx" #not necessary
     GENIUS_API_TOKEN="e-8UdRQNrIssPyM" # api token from genius.com (not necessary)
     TIME_API_KEY="QLLLDV7SWFD3" #not necessary
